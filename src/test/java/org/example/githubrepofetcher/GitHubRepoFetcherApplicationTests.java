a734c5f8f8a09e2fd40c1fca8de67bf7f45b66cf
Lucas <lupelikan@int.pl>
2025-04-04 03:17:04 +0300

    initialized springboot project with maven

---
+++ src/test/java/org/example/githubrepofetcher/GitHubRepoFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubrepofetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepoFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
